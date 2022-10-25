5e07107ab619eef345a6de1020b1a73d73a16080
kimiebi akah <kimiebi.akah@insight.com>
2022-10-25 16:54:27 -0400

    updated for proper naming of storage account

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Create Archive Storage account 
 resource "azurerm_storage_account" "storageacc1" {
-  name                              = "st-data-ai-user-${var.environment}"
+  name                              = "stdataaiuser${var.environment}"
   resource_group_name               = azurerm_resource_group.rg.name
   location                          = var.location
   account_tier                      = "Standard"
