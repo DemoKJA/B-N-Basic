bd8f45602bb9f4ceb90fa6e771062ce23e602352
kimiebi akah <kimiebi.akah@insight.com>
2022-10-25 17:06:38 -0400

    trying glboally unique

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "azurerm_storage_account" "storageacc1" {
 
 # Create Azure Datafactory
 resource "azurerm_data_factory" "adf" {
-  name                = "adf-${var.environment}"
+  name                = "adf-data-ai-${var.environment}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   identity {
