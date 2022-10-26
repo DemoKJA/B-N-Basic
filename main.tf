6a4b7cc1067b202f81cc98c55066f0ac96efa51e
kimiebi akah <kimiebi.akah@insight.com>
2022-10-26 11:32:23 -0400

    add

--- main.tf
+++ main.tf
@@ -46,6 +46,10 @@ resource "azurerm_storage_container" "storageacc_user_client_containers" {
   name                  = "testmain"
   storage_account_name  = azurerm_storage_account.storageacc_user.name
   container_access_type = "private"
+
+   lifecycle {
+    prevent_destroy = true
+  }
 }
 
 
